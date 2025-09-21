a9c3b4e2f2125fa4055356323648e4dfc0c1d5a5
Valentin Grushevskiy <vltn.grushevskiy@mail.ru>
2025-09-22 01:15:51 +0300

    feat: delete config

--- app/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-const DB_HOST = 'sql104.infinityfree.com';
-const DB_NAME = 'if0_39988229_task';
-const DB_USER = 'if0_39988229';
-const DB_PASS = 'jtmcVrjSk4';
-
-// php -S localhost:8000 -t public/
